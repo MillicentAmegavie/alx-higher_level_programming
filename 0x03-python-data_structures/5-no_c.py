18cf1bdf8a87a7ad223330b2ba6a37e8f420329b
Millicent Amegavie <miillicent.amegavie@yahoo.com>
2024-01-08 00:54:25 -0800

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     result = []
-    for l in my_string:
-        if l != 'c' and l != 'C':
-            result.append(l)
+    for letter in my_string:
+        if letter != 'c' and letter != 'C':
+            result.append(letter)
     return ''.join(result)
