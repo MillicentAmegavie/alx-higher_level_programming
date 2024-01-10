a1e1b9c28510c0cee0295b40760c3173c423532e
Millicent Amegavie <miillicent.amegavie@yahoo.com>
2024-01-10 06:45:21 -0800

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     result_matrix = matrix.copy()
-    for r in range(len(result_matrix)):
-        result_matrix[r] = list(map(lambda x: x ** 2, result_matrix[r]))
+    for row in range(len(result_matrix)):
+        result_matrix[row] = list(map(lambda x: x ** 2, result_matrix[row]))
     return result_matrix
 
