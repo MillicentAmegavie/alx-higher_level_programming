57c2f9c863d38899c6cbb107acd53f9cba408fa6
Millicent Amegavie <miillicent.amegavie@yahoo.com>
2024-01-23 04:13:59 -0800

    changes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -63,7 +63,7 @@ class Square:
         """Define the print() representation of a Square."""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
-        for m in range(0, self.__size):
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:
